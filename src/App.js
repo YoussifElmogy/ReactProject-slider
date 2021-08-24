3786567f4ee51dd23a2e98958d72b0d0bc3e821b
Youssef <abdelmonemyousef@gmail.com>
2021-08-24 14:18:28 +0200

    changes

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from 'react';
+import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
+import { FaQuoteRight } from 'react-icons/fa';
+import data from './data';
+function App() {
+  const [people,setpeople]=useState(data);
+  const [index,setindex]=useState(0);
+  useEffect(() => {
+    const lastIndex = people.length - 1;
+    if (index < 0) {
+      setindex(lastIndex);
+    }
+    if (index > lastIndex) {
+      setindex(0);
+    }
+  }, [index, people]);
+
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setindex(index + 1);
+    }, 5000);
+    return () => {
+      clearInterval(slider);
+    };
+  }, [index]);
+
+  return <main className="section">
+    <div className="title">
+     <h2>
+       <span>/</span>
+       reviews
+     </h2>
+    </div>
+    <div className="section-center">
+      {
+        people.map((person,personIndex)=>{
+          const { id, image, name, title, quote } = person;
+
+          let position = 'nextSlide';
+          if (personIndex === index) {
+            position = 'activeSlide';
+          }
+          if (
+            personIndex === index - 1 ||
+            (index === 0 && personIndex === people.length - 1)
+          ) {
+            position = 'lastSlide';
+          }
+
+
+          return( <article className={position}>
+            <img src={image} alt={name} className="person-img" />
+            <h4>{name}</h4>
+            <p className="title">{title}</p>
+            <p className="text">{quote}</p>
+            <FaQuoteRight className="icon" />
+          </article>
+          )
+        })
+      }
+      <button className="prev" onClick={() => setindex(index - 1)}>
+        <FiChevronLeft />
+      </button>
+      <button className="next" onClick={() => setindex(index + 1)}>
+        <FiChevronRight />
+      </button>
+    </div>
+  </main>
+}
+
+export default App;
